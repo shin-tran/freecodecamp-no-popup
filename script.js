e1f026c042992afa69999f003aaaf7bf7b5787d5
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-03-12 20:00:55 +0700

    Update namespace and add support URL in script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Freecodecamp No Popup
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/shin-tran
 // @version      0.1
 // @description  Removes the donate popup on FreeCodeCamp
 // @author       ngocshintran
 // @match        https://www.freecodecamp.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=freecodecamp.org
 // @grant        GM_addStyle
+// @supportURL   https://github.com/shin-tran/freecodecamp-no-popup/issues
 // @license MIT
 // ==/UserScript==
 
